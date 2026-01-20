12439ae1013d1f39289962add67af862ea4d424b
MR-Amr-Emam <amradar757@gmail.com>
2026-01-20 12:54:38 +0200

    initial commit

---
+++ models/AuthenticationUser.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace blog_app_ai_dotnet.models;
+
+public partial class AuthenticationUser
+{
+    public long Id { get; set; }
+
+    public string Password { get; set; } = null!;
+
+    public DateTime? LastLogin { get; set; }
+
+    public bool IsSuperuser { get; set; }
+
+    public string Username { get; set; } = null!;
+
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public bool IsStaff { get; set; }
+
+    public bool IsActive { get; set; }
+
+    public DateTime DateJoined { get; set; }
+
+    public string? Bio { get; set; }
+
+    public string ProfileImage { get; set; } = null!;
+
+    public string BackgroundImage { get; set; } = null!;
+
+    public int FriendsNumber { get; set; }
+}
---
+++ models/dbcontext.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql.Scaffolding.Internal;
+
+namespace blog_app_ai_dotnet.models;
+
+public partial class dbcontext : DbContext
+{
+    public dbcontext()
+    {
+    }
+
+    public dbcontext(DbContextOptions<dbcontext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<AuthenticationUser> AuthenticationUsers { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseMySql("server=127.0.0.1;port=3306;database=blog-app;uid=root;pwd=amr_emam", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.43-mysql"));
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .UseCollation("utf8mb4_0900_ai_ci")
+            .HasCharSet("utf8mb4");
+
+        modelBuilder.Entity<AuthenticationUser>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("authentication_user");
+
+            entity.HasIndex(e => e.Username, "username").IsUnique();
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.BackgroundImage)
+                .HasMaxLength(100)
+                .HasColumnName("background_image");
+            entity.Property(e => e.Bio)
+                .HasMaxLength(100)
+                .HasColumnName("bio");
+            entity.Property(e => e.DateJoined)
+                .HasMaxLength(6)
+                .HasColumnName("date_joined");
+            entity.Property(e => e.Email)
+                .HasMaxLength(254)
+                .HasColumnName("email");
+            entity.Property(e => e.FirstName)
+                .HasMaxLength(150)
+                .HasColumnName("first_name");
+            entity.Property(e => e.FriendsNumber).HasColumnName("friends_number");
+            entity.Property(e => e.IsActive).HasColumnName("is_active");
+            entity.Property(e => e.IsStaff).HasColumnName("is_staff");
+            entity.Property(e => e.IsSuperuser).HasColumnName("is_superuser");
+            entity.Property(e => e.LastLogin)
+                .HasMaxLength(6)
+                .HasColumnName("last_login");
+            entity.Property(e => e.LastName)
+                .HasMaxLength(150)
+                .HasColumnName("last_name");
+            entity.Property(e => e.Password)
+                .HasMaxLength(128)
+                .HasColumnName("password");
+            entity.Property(e => e.ProfileImage)
+                .HasMaxLength(100)
+                .HasColumnName("profile_image");
+            entity.Property(e => e.Username)
+                .HasMaxLength(150)
+                .HasColumnName("username");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
